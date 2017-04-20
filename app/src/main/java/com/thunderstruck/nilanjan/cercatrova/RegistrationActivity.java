e8896dc047fa6c102ba7732d6bd3aa7e57ff29bb
Debapriya <paul.debapriya20@gmail.com>
2017-04-20 22:13:59 +0530

    Registration UI done

---
+++ app/src/main/java/com/thunderstruck/nilanjan/cercatrova/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.thunderstruck.nilanjan.cercatrova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
